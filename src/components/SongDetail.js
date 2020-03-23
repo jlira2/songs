af669397a850e41e2b19f5bacb41ac20b020d180
Lira, Moises <jesuslira87@gmail.com>
2020-03-23 16:12:22 -0600

    finished songs app

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -2,8 +2,19 @@ import React from "react";
 import { connect } from "react-redux";
 
 const SongDetail = props => {
-	console.log(props);
-	return <div>SongDetail</div>;
+	if (!props.song) {
+		return <div>Select a Song</div>;
+	}
+	return (
+		<div>
+			<h3>Details for:</h3>
+			<p>
+				Title: {props.song.title}
+				<br />
+				Duration: {props.song.duration}
+			</p>
+		</div>
+	);
 };
 
 const mapStateToProps = state => {
